13c9f7dc31fde98c068ad43cfd3b8a94ae6790c6
Alam Guardin <alamguardin@gmail.com>
2025-11-27 19:59:00 -0500

    feat: implement toast display functionality and restructure HTML layout

---
+++ src/main.js
@@ -0,0 +1,14 @@
+import './style.css';
+import { toast } from '../lib';
+
+const btn = document.querySelector('#defaultButton');
+
+let count = 1;
+btn.addEventListener('click', () => {
+	toast.display(`This is the toast #${count} from wc-sonner`, {
+		description: 'This is a toast description',
+		closeButton: true,
+	});
+
+	count++;
+});
