081d5bdffdc8b197b0c34899ced7d42380749e3e
Alam Guardin <alamguardin@gmail.com>
2025-11-30 16:18:05 -0500

    feat: add styles to toast variants

--- src/main.js
+++ src/main.js
@@ -5,7 +5,7 @@ const btn = document.querySelector('#defaultButton');
 
 let count = 1;
 btn.addEventListener('click', () => {
-	toast.info(`This is the toast #${count} from wc-sonner`, {
+	toast.display(`This is the toast #${count} from wc-sonner`, {
 		description: 'This is a toast description',
 		closeButton: true,
 	});
