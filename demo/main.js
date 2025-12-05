e8ab827b23c67d9450649acb350a5a43321dc93c
Alam Guardin <alamguardin@gmail.com>
2025-12-04 22:12:44 -0500

    fix: update toast component positioning and improve animation handling

--- demo/main.js
+++ demo/main.js
@@ -8,6 +8,7 @@ btn.addEventListener('click', () => {
 	toast.display(`This is the toast #${count} from wc-beru`, {
 		description: 'This is a toast description',
 		closeButton: true,
+		beruId: 'second',
 	});
 
 	count++;
